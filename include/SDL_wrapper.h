db8ae1eed54d14feedb2f4838e755de9ae2110a6
Veerendra Shirole <veerendrashirole@gmail.com>
2014-02-23 20:04:54 -0800

    - Added SDL library to repo - Tested creating a SDL Window in SDL_wrapper.cpp - Does NOT accept any event!

--- include/SDL_wrapper.h
+++ include/SDL_wrapper.h
@@ -2,10 +2,24 @@
 #define __include__sdl_wrapper__
 
 #include "lib_wrapper.h"
+#include "SDL.h"
 
 class SdlWrapper : public LibWrapper {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  static const int SCREEN_WIDTH = 640 ;
+  static const int SCREEN_HEIGHT = 480 ;
 
 public:
+
+  SdlWrapper():window(NULL), renderer(NULL) {}  // Default constructor
+
+  ~SdlWrapper() {                               // Default destructor
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+  }
+
   bool init();
   void render();
 };
--- src/SDL_wrapper.cpp
+++ src/SDL_wrapper.cpp
@@ -1,10 +1,35 @@
-
+#include <iostream>
 #include "sdl_wrapper.h"
 
+using namespace std ;
+
 bool SdlWrapper::init() {
+
+  // TODO (Veerendra): return meaningful error values
+  int quit = false ;
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) == -1){
+    cout << SDL_GetError() << endl;
+    return false;
+  }
+
+  window = SDL_CreateWindow("SpellingBum", SDL_WINDOWPOS_CENTERED, 
+    SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (window == nullptr){
+    cout << SDL_GetError() << endl;
+    return false;
+  }
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED 
+    | SDL_RENDERER_PRESENTVSYNC);
+  if (renderer == nullptr){
+    cout << SDL_GetError() << endl;
+    return false;
+  }
+
+  SDL_RenderClear(renderer);
   return true;
 }
 
 void SdlWrapper::render() {
-  
+
 }
